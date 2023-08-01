7fd7b9feae355ce00eda3691a86432eb9349d72d
jose suarez <70371111+tucajas@users.noreply.github.com>
2023-08-01 15:58:13 -0300

    presupuesto

--- src/app/presupuesto/pages/agregar/agregar.component.ts
+++ src/app/presupuesto/pages/agregar/agregar.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Articulo } from 'src/app/articulo/interface';
 import { ArticuloService } from 'src/app/articulo/servicios/articulo.service';
+import { TipoTrabajo } from 'src/app/tipotrabajo/interface';
+import { TipotrabajoService } from 'src/app/tipotrabajo/tipotrabajo.service';
 
 @Component({
   selector: 'app-agregar',
@@ -11,29 +13,37 @@ import { ArticuloService } from 'src/app/articulo/servicios/articulo.service';
 export class AgregarComponent {
 
   precioVenta:number=0;
-  precioVenta2:number=0;
+  
   cantidad:number=0;
-  cantidad2:number=0;
+
   subtotal:number=0;
   articulos:Articulo[]=[];
+  servicio:TipoTrabajo[]=[];
   subtotal2:number=0;
   subtotal3:number=0;
   total:number=0;
 
   
 
-  constructor( private articuloService: ArticuloService ) { }
+  constructor( private articuloService: ArticuloService,private servicios: TipotrabajoService ) { }
   ngOnInit(): void {
-
-    this.articuloService.getArticulo()
-    .subscribe( articulos => this.articulos=articulos);
+    this.servicios.getTipoTrabajo().subscribe(resp=>this.servicio=resp);
+    this.articuloService.getArticulo().subscribe( articulos => this.articulos=articulos);
 
   }
-  
-  sumar(){
-    this.subtotal=this.precioVenta*this.cantidad;
-    this.subtotal2=this.precioVenta2*this.cantidad2;
+  tarjeta(){
+    this.subtotal2=this.subtotal*25/100;
     this.total=this.subtotal+this.subtotal2;
+  }
+  efectivo(){
+    const porcenDesc=this.subtotal*80/100;
+    this.subtotal2=this.subtotal-porcenDesc;
+    this.total=this.subtotal-this.subtotal2;
+  }
+  sumar(){
+    
+    this.subtotal=this.precioVenta*this.cantidad;
+   
   }
   
 
